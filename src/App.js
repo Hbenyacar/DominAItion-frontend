457e3276fcab26a13171ca1792d02995ce42781e
mlenkeit4 <112277098+mlenkeit4@users.noreply.github.com>
2025-10-08 15:07:16 -0400

    Added sample page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Profile from './scenes/Profile/Profile';
 import Landing from './scenes/Landing/Landing';
 import AvatarSelect from './scenes/Avatar/Avatar';
 import Europe from './widgets/Maps/USA/Europe';
+import Sample from './scenes/Sample/Sample';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Route path='/' element={<Landing/>} />
       <Route path='/avatar' element={<AvatarSelect/>} />
       <Route path='/europe' element={<Europe/>} />
+      <Route path='/sample' element={<Sample/>} />
     </Routes>
   );
 }
